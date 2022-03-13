c96d3edea9779cb32ad303eb6892a34a5e865d40
umesh chauda <umeshgupta821@gmail.com>
2022-03-13 12:18:19 +0530

    Added IGenericRepository interface and implemented it with generic repository

---
+++ HotelListing/IRepository/IGenericRepositorycs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.IRepository
+{
+    public interface IGenericRepositorycs<T> where T : class
+    {
+        Task<IList<T>> GetAll(
+            Expression<Func<T, bool>> expression,
+            Func<IQueryable<T>, IOrderedEnumerable<T>> orderBy = null,
+            List<string> includes = null);
+
+        Task<T> Get(
+            Expression<Func<T, bool>> expression = null,
+            List<string> includes = null);
+
+        Task<T> Insert(T entity);
+        Task<T> InsertRange(IEnumerable<T> entities);
+        Task Delete(int id);
+        void DeleteRange(IEnumerable<T> entities);
+        void Update(T Entity);
+
+    }
+}
---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using HotelListing.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepositorycs<T> where T : class
+    {
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(Expression<Func<T, bool>> expression = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAll(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IOrderedEnumerable<T>> orderBy = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> InsertRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
